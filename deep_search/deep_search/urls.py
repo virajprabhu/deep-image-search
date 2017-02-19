b178757bef8279d5d52ac39e016364d781827f1c
Deshraj <deshrajdry@gmail.com>
2017-02-18 20:47:59 -0500

    Added url patterns for VQA and captioning

--- deep_search/deep_search/urls.py
+++ deep_search/deep_search/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^api/vqa', include('vqa.urls', namespace='vqa')),
+    url(r'^api/captioning', include('captioning.urls', namespace='captioning')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
